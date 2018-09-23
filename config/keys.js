bfa9cc6dd39c0c9751162d24971eaf278ecf2cdc
SenhorLucas <lucas.nviana@gmail.com>
2018-09-23 21:19:29 +0200

    did I add node_modules?

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	google_client_ID:
+		"449665497801-p8muj2s7okdnht004mqiibe32k0etg3n.apps.googleusercontent.com",
+	google_client_secret: "rP_rTq9SCZ5tyoZ3ZwsQy6YZ"
+};
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require("express");
+// https://shielded-falls-26186.herokuapp.com/ |
+// https://git.heroku.com/shielded-falls-26186.git
 
+// git push heroku master
+// heroku logs
+const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send({ hi: "there" });
-});
+require("./services/passport");
+require("./routes/authroutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
