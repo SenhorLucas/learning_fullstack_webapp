f2d8acd9064bd6e0e725f407c13135859ffbe505
SenhorLucas <lucas.nviana@gmail.com>
2018-09-29 00:17:41 +0200

    made sure a user is only added once to the database

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,7 @@ const GoogleStrategy = require("passport-google-oauth20");
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-const User = mongoose.model("users");
+const User = mongoose.model("User");
 
 passport.use(
 	new GoogleStrategy(
@@ -13,7 +13,15 @@ passport.use(
 			callbackURL: "/auth/google/callback"
 		},
 		(accessToken, refreshToken, profile, done) => {
-			new User({ google_id: profile.id }).save();
+			User.findOne({ google_id: profile.id }).then(existingUser => {
+				if (existingUser) {
+					done(null, existingUser);
+				} else {
+					new User({ google_id: profile.id })
+						.save()
+						.then(user => done(null, user));
+				}
+			});
 		}
 	)
 );
