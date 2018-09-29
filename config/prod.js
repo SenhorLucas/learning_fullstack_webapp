6a13863ee532e152afb0ffb4dd5d309a72be9a07
SenhorLucas <lucas.nviana@gmail.com>
2018-09-29 17:35:50 +0200

    configured development and production environments

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// dev.js - don't commit this
+module.exports = {
+	google_client_ID: process.env.GOOGLE_CLIENT_ID,
+	google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
+	mongo_uri: process.env.MONGO_URI,
+	cookie_key: process.env.COOKIE_KEY
+};
