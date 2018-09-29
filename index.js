62baf0944a9e5b8ceabcd955f19393ceac2c80a2
SenhorLucas <lucas.nviana@gmail.com>
2018-09-29 15:23:11 +0200

    added an html file for google oauth domain ownership verification

--- index.js
+++ index.js
@@ -5,10 +5,25 @@
 // heroku logs
 const express = require("express");
 const app = express();
-const keys = require("./config/keys");
 const mongoose = require("mongoose");
+const cookie_session = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
 
-mongoose.connect(keys.mongo_uri);
+mongoose.connect(
+	keys.mongo_uri,
+	{ useNewUrlParser: true }
+);
+
+app.use(
+	cookie_session({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookie_key]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./models/users.js");
 require("./services/passport");
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require("../config/keys");
 
 const User = mongoose.model("User");
 
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((user_id, done) => {
+	User.findById(user_id).then(user => {
+		done(null, user);
+	});
+});
+
 passport.use(
 	new GoogleStrategy(
 		{
