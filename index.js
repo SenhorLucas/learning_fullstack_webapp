93fd06aff7cad1a3f665ef9ef63304243d86e85d
SenhorLucas <lucas.nviana@gmail.com>
2018-09-29 16:04:26 +0200

    another attempt

--- index.js
+++ index.js
@@ -5,29 +5,29 @@
 // heroku logs
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const cookie_session = require("cookie-session");
-const passport = require("passport");
-const keys = require("./config/keys");
+// const mongoose = require("mongoose");
+// const cookie_session = require("cookie-session");
+// const passport = require("passport");
+// const keys = require("./config/keys");
 
-mongoose.connect(
-	keys.mongo_uri,
-	{ useNewUrlParser: true }
-);
+// mongoose.connect(
+// 	keys.mongo_uri,
+// 	{ useNewUrlParser: true }
+// );
 
-app.use(
-	cookie_session({
-		maxAge: 30 * 24 * 60 * 60 * 1000,
-		keys: [keys.cookie_key]
-	})
-);
+// app.use(
+// 	cookie_session({
+// 		maxAge: 30 * 24 * 60 * 60 * 1000,
+// 		keys: [keys.cookie_key]
+// 	})
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-require("./models/users.js");
-require("./services/passport");
-require("./routes/authroutes")(app);
+// require("./models/users.js");
+// require("./services/passport");
+// require("./routes/authroutes")(app);
 
 app.use(express.static("/"));
 
