3d117cb29e260b5cf605d5f549d198166d99c4bb
SenhorLucas <lucas.nviana@gmail.com>
2018-11-24 00:48:34 +0100

    rounded up billing

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const cookie_session = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
 
 mongoose.connect(
-	keys.mongo_uri,
-	{ useNewUrlParser: true }
+  keys.mongo_uri,
+  { useNewUrlParser: true }
 );
 
+app.use(bodyParser.json());
+
 app.use(
-	cookie_session({
-		maxAge: 30 * 24 * 60 * 60 * 1000,
-		keys: [keys.cookie_key]
-	})
+  cookie_session({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookie_key]
+  })
 );
 
 app.use(passport.initialize());
@@ -23,6 +26,7 @@ app.use(passport.session());
 require("./models/users.js");
 require("./services/passport");
 require("./routes/authroutes")(app);
+require("./routes/billingroutes")(app);
 require("./routes/google_domain_verification")(app);
 
 const PORT = process.env.PORT || 5000;
--- models/users.js
+++ models/users.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	google_id: String
+  google_id: String,
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model("User", userSchema);
